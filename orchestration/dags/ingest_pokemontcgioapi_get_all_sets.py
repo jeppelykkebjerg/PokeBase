4292c2d136c926b332765d1a5f076672d9d36c93
Jeppe Lykkebjerg Jensen <jeppelykkebjerg@gmail.com>
2024-08-04 21:52:20 +0200

    added orchestrator and migrated two pipelines - sets and cards

---
+++ orchestration/dags/ingest_pokemontcgioapi_get_all_sets.py
@@ -0,0 +1,52 @@
+from airflow.decorators import dag, task
+from dotenv import load_dotenv
+import os
+import pokemontcgsdk
+import pandas as pd
+import uuid
+from datetime import datetime
+
+# Define the basic parameters of the DAG, like schedule and start_date
+@dag(
+    start_date=datetime(2024, 8, 1),
+    schedule="@daily",
+    catchup=False,
+    default_args={"owner": "Jeppe Lykkebjerg Jensen", "retries": 3},
+    tags=["ingestion"],
+)
+def ingest_pokemontcgioapi_get_all_sets():
+    # Load environment variables
+    load_dotenv()
+    POKEMONTCG_IO_API_KEY = os.getenv("POKEMONTCG_IO_API_KEY")
+    
+    # script variables
+    pokemontcgioapi_object = 'sets'
+
+    @task
+    def get_sets():
+        pokemon_sets_list = pokemontcgsdk.Set.all()
+        pokemon_sets_df = pd.DataFrame(pokemon_sets_list)
+
+        return pokemon_sets_df
+    
+    @task
+    def write_sets_dataframe_to_json(data_frame, pokemontcgioapi_object, uuid):
+        #Persist as json file
+        df = data_frame
+        #Persist as json file
+        write_path = f'/usr/local/airflow/data/pokemontcgioapi/{pokemontcgioapi_object}'
+
+        # Check if the directory already exists
+        if not os.path.exists(write_path):
+            os.makedirs(write_path)
+            print(f"{write_path} created successfully!")
+
+        write_path_json = f'{write_path}/{pokemontcgioapi_object}_{uuid}.json'
+        df.to_json(write_path_json, orient='records')
+
+    # Invoke functions to create tasks and define dependencies
+    pokemon_sets_df = get_sets()
+    write_sets_dataframe_to_json(pokemon_sets_df, pokemontcgioapi_object, uuid.uuid4())
+
+# Instantiate the DAG
+ingest_pokemontcgioapi_get_all_sets()
