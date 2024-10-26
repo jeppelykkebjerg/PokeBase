d18078a21c4c46003d57c512db78abb426fc2f8d
Jeppe Lykkebjerg Jensen <mymail@gmail.com>
2024-10-26 14:04:44 +0200

    Stuff

---
+++ PokedexApp/PokedexEventProducer.py
@@ -0,0 +1,8 @@
+import PokemonTrainers as pt
+import time
+
+while True:
+    t_id: str = pt.GetTrainerId()
+    print(t_id)
+
+    time.sleep(0.5)
--- PokedexApp/PokemonTrainers.py
+++ PokedexApp/PokemonTrainers.py
@@ -1,9 +1,16 @@
 import random
 
-def ReturnTrainerId() -> str:
+def GetTrainerId() -> str:
+    """
+        We have 50 trainers in our trainer-union.
+        This function returns a random number between 1 and 50 (inclusive) to provide the trainer-id variable
+        in a pokedex event.
+
+        Arguments: (none)
+
+        Returns: str
+    """
     trainer_id: int = random.randint(1,51)
     trainer_id_str = f"TID-{trainer_id}"
+
     return trainer_id_str
-
-
-print(ReturnTrainerId())
